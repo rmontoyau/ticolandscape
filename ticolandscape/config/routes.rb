1d7f166491b08be27dc4516302f5080caf679d4a
rmontoyau <rmontoyau@gmail.com>
2017-05-01 16:46:02 -0600

    begining the user template

--- ticolandscape/config/routes.rb
+++ ticolandscape/config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+
+
+  root 'home#index'
+
+	resources :home do
+		collection do
+			get :about
+			get :contactus
+		end
+	end
+
+	namespace :admin do
+		resource :users do
+			collection do
+				get :index
+				get :new
+				post :create
+				get :edit
+				post :update
+				get :delete
+
+			end# collection
+		end #users
+	end #admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
