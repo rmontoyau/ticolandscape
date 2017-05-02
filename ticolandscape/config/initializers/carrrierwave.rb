7f4a2f8c28c8d59d580a6dc796f3d260ed328a14
Rudy Montoya <rmontoyau@gmail.com>
2017-05-02 11:10:16 -0600

    Update carrrierwave.rb

--- ticolandscape/config/initializers/carrrierwave.rb
+++ ticolandscape/config/initializers/carrrierwave.rb
@@ -2,7 +2,7 @@ Carrierwave.configure do |config|
   config.fog_credentials = {
     provider:                  'AWS',
     aws_access_key_id:         ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key_id:  ENV['AWS_SECRET_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
     region:                    ''
   }
   #for testing, upload files to local 'tmp' folder
